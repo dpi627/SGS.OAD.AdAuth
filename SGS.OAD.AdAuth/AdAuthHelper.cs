86347ac96ba1b8e844f733a652a8fe61c76b5d18
brian_li <brian.li@sgs.com>
2024-07-22 13:46:31 +0800

    finish package 1st version

---
+++ SGS.OAD.AdAuth/AdAuthHelper.cs
@@ -0,0 +1,51 @@
+﻿using SGS.OAD.AdAuth.Models;
+using System.DirectoryServices.AccountManagement;
+
+namespace SGS.OAD.AdAuth;
+
+public class AdAuthHelper
+{
+    /// <summary>
+    /// 驗證使用者帳號密碼是否正確
+    /// </summary>
+    /// <param name="UserId">帳號</param>
+    /// <param name="Password">密碼</param>
+    /// <param name="Domain">AD域名</param>
+    /// <returns></returns>
+    public static bool IsValid(string UserId, string Password, string Domain = "APAC")
+    {
+        using PrincipalContext context = new(ContextType.Domain, Domain);
+        return context.ValidateCredentials(UserId, Password);
+    }
+
+    /// <summary>
+    /// 取得使用者資訊
+    /// </summary>
+    /// <param name="UserId">帳號</param>
+    /// <param name="Password">密碼</param>
+    /// <param name="Domain">AD域名</param>
+    /// <returns></returns>
+    public static AdInfoModel? GetInfo(string UserId, string Password, string Domain = "APAC")
+    {
+        using PrincipalContext context = new(ContextType.Domain, Domain);
+
+        bool vaild = context.ValidateCredentials(UserId, Password);
+
+        if (!vaild)
+            return null;
+
+        UserPrincipal user = UserPrincipal.FindByIdentity(context, UserId);
+
+        if (user == null)
+            return null;
+
+        return new AdInfoModel() {
+            Enabled = user.Enabled,
+            Name = user.Name,
+            EmployeeId = user.EmployeeId,
+            DisplayName = user.DisplayName,
+            Description = user.Description,
+            EmailAddress = user.EmailAddress
+        };
+    }
+}
--- SGS.OAD.AdAuth/Models/AdInfoModel.cs
+++ SGS.OAD.AdAuth/Models/AdInfoModel.cs
@@ -1,11 +1,32 @@
 ﻿namespace SGS.OAD.AdAuth.Models;
 
+/// <summary>
+/// 用於儲存 AD 使用者資訊
+/// </summary>
 public class AdInfoModel
 {
+    /// <summary>
+    /// 是否啟用
+    /// </summary>
     public bool? Enabled { get; set; }
-    public string? DisplayName { get; set; }
-    public string? EmailAddress { get; set; }
+    /// <summary>
+    /// 使用者名稱，例如 Brian_Li
+    /// </summary>
     public string? Name { get; set; }
-    public string? Description { get; set; }
+    /// <summary>
+    /// 員工編號
+    /// </summary>
     public string? EmployeeId { get; set; }
+    /// <summary>
+    /// 顯示名稱，例如 Brian Li
+    /// </summary>
+    public string? DisplayName { get; set; }
+    /// <summary>
+    /// 描述，可能會取得部門資料，例如 TW_TPE_LABADM
+    /// </summary>
+    public string? Description { get; set; }
+    /// <summary>
+    /// 電子郵件
+    /// </summary>
+    public string? EmailAddress { get; set; }
 }
