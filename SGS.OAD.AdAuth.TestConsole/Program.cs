ac7f471075a784287eb6791b8a725aa9f7ada436
brian_li <brian.li@sgs.com>
2024-07-22 13:48:02 +0800

    add test console app

---
+++ SGS.OAD.AdAuth.TestConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SGS.OAD.AdAuth.TestConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
