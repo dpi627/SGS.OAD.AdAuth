7cbff5a6e356568345b7c7cc507f2f3787bf53e3
brian_li <brian.li@sgs.com>
2024-07-22 13:06:42 +0800

    add ad info model

---
+++ SGS.OAD.AdAuth/Models/AdInfoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SGS.OAD.AdAuth.Models;
+
+public class AdInfoModel
+{
+    public bool? Enabled { get; set; }
+    public string? DisplayName { get; set; }
+    public string? EmailAddress { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? EmployeeId { get; set; }
+}
