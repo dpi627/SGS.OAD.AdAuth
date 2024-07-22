2cb02c88904a772788c8186d07e9cf79236efc2e
brian_li <brian.li@sgs.com>
2024-07-22 15:17:51 +0800

    finish test console with config settings and user secrets

--- SGS.OAD.AdAuth.TestConsole/Program.cs
+++ SGS.OAD.AdAuth.TestConsole/Program.cs
@@ -1,10 +1,58 @@
-﻿namespace SGS.OAD.AdAuth.TestConsole
+﻿using Microsoft.Extensions.Configuration;
+using SGS.OAD.AdAuth.Models;
+using System.Reflection;
+using System.Text;
+
+namespace SGS.OAD.AdAuth.TestConsole;
+
+internal class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        // 讀取組態檔
+        IConfiguration config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddUserSecrets<Program>() //如果有 User Secrets 優先讀取
+            .Build();
+
+        string uid = config["UserID"]!;
+        string pwd = config["Password"]!;
+        //string dmn = config["Domain"]!; // 預設為 APAC，如非此域名則需要設定
+
+        // 驗證帳號密碼是否有效
+        bool valid = AdAuthHelper.IsValid(uid, pwd);
+        Console.WriteLine(valid);
+
+        // 取得使用者資訊，會先驗證，如驗證無效取得 null
+        AdInfoModel? info = AdAuthHelper.GetInfo(uid, pwd);
+
+        if (info != null)
+            Console.WriteLine(Serialize<AdInfoModel>(info));
+        else
+            Console.WriteLine("查無資料");
     }
+
+    #region other methods
+    /// <summary>
+    /// 序列化類別之屬性與值
+    /// </summary>
+    public static string Serialize<T>(T model)
+    {
+        var sb = new StringBuilder();
+        var properties = model.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        foreach (var prop in properties)
+        {
+            var value = prop.GetValue(model);
+            if (value != null)
+            {
+                if (sb.Length > 0) sb.AppendLine();
+                sb.Append($"\"{prop.Name}\":\"{value}\"");
+            }
+        }
+
+        return sb.ToString();
+    }
+    #endregion
 }
