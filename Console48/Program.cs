3a8681bf620d05a201a3774782a69d2bbd9785f0
brian_li <brian.li@sgs.com>
2024-12-07 14:34:30 +0800

    add demo console fot net 4.8

---
+++ Console48/Program.cs
@@ -0,0 +1,14 @@
+﻿using SGS.OAD.AdAuth;
+using System;
+
+namespace Console48
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var isVaild = AdAuthHelper.IsValid("YourAdAccount", "YourAdPassword");
+            Console.WriteLine($"{isVaild}");
+        }
+    }
+}
