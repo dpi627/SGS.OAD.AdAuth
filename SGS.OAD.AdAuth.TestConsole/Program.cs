fee946ebad4e7929fb83c9d18e5d093a4be3c7c1
brian_li <brian.li@sgs.com>
2025-02-03 16:57:36 +0800

    feat: 支援 UTF-8 編碼並讀取設定檔
    - 設定主控台的輸入和輸出編碼為 UTF-8，以支援 UTF-8 字元。
    - 從 `appsettings.json` 和使用者機密讀取設定。

--- SGS.OAD.AdAuth.TestConsole/Program.cs
+++ SGS.OAD.AdAuth.TestConsole/Program.cs
@@ -9,6 +9,9 @@ internal class Program
 {
     static void Main()
     {
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
         // 讀取組態檔
         IConfiguration config = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
