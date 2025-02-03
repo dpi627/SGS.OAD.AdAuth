f212caff2cfb00fffdf344f93c22bfca156b963f
brian_li <brian.li@sgs.com>
2025-02-03 16:56:03 +0800

    feat: 新增 GetHrInfo 方法於 HrService.cs
    - 在 SGS.OAD.AdAuth 命名空間內的 HrService.cs 檔案中新增 GetHrInfo 方法，從資料庫的 Employee 表中取得指定 AD 帳號的 HR 資訊。
    - GetHrInfo 方法構建 SQL 查詢，選取 ADAccount 符合參數且 QUITDATE 為 0 或未來日期的首筆記錄。
    - 使用 SqlConnection 連接資料庫，並使用 SqlCommand 執行查詢，透過 SqlDataReader 讀取結果並映射到新的 HrInfoModel 物件。
    - 若無符合記錄，方法將返回新的 HrInfoModel 實例。
    - 新增私有靜態方法 GetDateTime 以將資料庫值轉換為 DateTime? 物件，處理值為 DBNull 或零的情況。

--- SGS.OAD.AdAuth/HrService.cs
+++ SGS.OAD.AdAuth/HrService.cs
@@ -52,5 +52,59 @@ namespace SGS.OAD.AdAuth
 
             return stfCode ?? "";
         }
+
+        public HrInfoModel GetHrInfo(string adAccount)
+        {
+            string query = @"
+                SELECT TOP 1 stf_code, stf_cname, TitleName, QUITDATE, JOBSTATUS, JOBSTATUS_Text
+                FROM Employee (nolock)
+                WHERE ADAccount = @adAccount AND (QUITDATE = 0 OR QUITDATE >= @today)
+            ";
+            HrInfoModel? hrInfo = null;
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@adAccount", adAccount);
+                        command.Parameters.AddWithValue("@today", DateTime.Now.ToString("yyyyMMdd"));
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                hrInfo = new HrInfoModel
+                                {
+                                    stf_code = reader["stf_code"] as string,
+                                    stf_cname = reader["stf_cname"] as string,
+                                    TitleName = reader["TitleName"] as string,
+                                    QUITDATE = GetDateTime(reader["QUITDATE"]),
+                                    JOBSTATUS = reader["JOBSTATUS"] as string,
+                                    JOBSTATUS_Text = reader["JOBSTATUS_Text"] as string
+                                };
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+
+            return hrInfo ?? new HrInfoModel();
+        }
+
+        private static DateTime? GetDateTime(object val)
+        {
+            if (val != DBNull.Value && Convert.ToInt32(val) != 0)
+            {
+                return DateTime.ParseExact(val.ToString(), "yyyyMMdd", null);
+            }
+            return null;
+        }
     }
 }
