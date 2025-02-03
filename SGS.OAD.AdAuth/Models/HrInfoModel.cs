7e60184177e1ef30fb6a38705bcb83010f4c487a
brian_li <brian.li@sgs.com>
2025-02-03 16:55:44 +0800

    feat: 新增 SGS.OAD.AdAuth 命名空間及 HrInfoModel 類別
    - 新增 SGS.OAD.AdAuth 命名空間。
    - 在該命名空間中定義 HrInfoModel 類別。
    - HrInfoModel 類別包含六個屬性：
      - stf_code (可空字串) 員工代碼。
      - stf_cname (可空字串) 員工姓名。
      - TitleName (可空字串) 職稱名稱。
      - QUITDATE (可空 DateTime) 離職日期。
      - JOBSTATUS (可空字串) 工作狀態。
      - JOBSTATUS_Text (可空字串) 工作狀態文本。

---
+++ SGS.OAD.AdAuth/Models/HrInfoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SGS.OAD.AdAuth
+{
+    public class HrInfoModel
+    {
+        public string? stf_code { get; set; }
+        public string? stf_cname { get; set; }
+        public string? TitleName { get; set; }
+        public DateTime? QUITDATE { get; set; }
+        public string? JOBSTATUS { get; set; }
+        public string? JOBSTATUS_Text { get; set; }
+    }
+}
