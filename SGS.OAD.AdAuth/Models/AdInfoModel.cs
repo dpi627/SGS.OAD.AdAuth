2753dfd983a819963e43b9df758a6a66b60c5683
brian_li <brian.li@sgs.com>
2025-02-05 16:13:41 +0800

    refactor!: 移除 AdInfoModel 中的 EmployeeId 和 CName 屬性
    - 移除 EmployeeId 屬性，該屬性用於存儲員工 ID。
    - 移除 CName 屬性，該屬性用於存儲來自 HR 資料的中文姓名。

--- SGS.OAD.AdAuth/Models/AdInfoModel.cs
+++ SGS.OAD.AdAuth/Models/AdInfoModel.cs
@@ -14,10 +14,6 @@ public class AdInfoModel
     /// </summary>
     public string? Name { get; set; }
     /// <summary>
-    /// 員工編號
-    /// </summary>
-    public string? EmployeeId { get; set; }
-    /// <summary>
     /// 顯示名稱，例如 Brian Li
     /// </summary>
     public string? DisplayName { get; set; }
@@ -29,8 +25,4 @@ public class AdInfoModel
     /// 電子郵件
     /// </summary>
     public string? EmailAddress { get; set; }
-    /// <summary>
-    /// 中文姓名，取自HR資料(過渡時期使用)
-    /// </summary>
-    public string? CName { get; set; }
 }
